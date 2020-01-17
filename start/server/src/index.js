927f8b4e23b3174a1b89f4e0800b16fa161706fe
Leon (Ral) Mayer <48362631+ralmayer@users.noreply.github.com>
2020-01-17 13:51:35 +0400

    Update index.js

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -22,7 +22,6 @@ const server = new ApolloServer({
     const email = Buffer.from(auth, 'base64').toString('ascii');
     if (!isEmail.validate(email)) return { user: null };
     // find a user by their email
-    // find a user by their email
     const users = await store.users.findOrCreate({ where: { email } });
     const user = users && users[0] || null;
 
